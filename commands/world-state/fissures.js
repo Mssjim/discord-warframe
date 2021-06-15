bfe5cb269c27bfe2ca3827fb72bf9e22e008a899
Mssjim <mrmssjim@gmail.com>
2021-06-15 15:56:24 -0300

    Added 'fisssures' command

---
+++ commands/world-state/fissures.js
@@ -0,0 +1,56 @@
+const { MessageEmbed } = require("discord.js");
+const fecther = require("../../resources/fecther");
+const pagination = require("discord.js-pagination");
+
+module.exports = {
+    name: 'fissures',
+    aliases: ['fissure', 'relics', 'relic'],
+    description: 'Get the list of all Void Fissure missions',
+    category: 'world-state',
+
+    run: async(client, msg, args) => {
+        const data = await fecther('https://api.warframestat.us/pc/fissures');
+
+        if(data) {
+            const pages = [];
+            const eras = {
+                lith: [],
+                meso: [],
+                neo: [],
+                axi: [],
+                requiem: [],
+            };
+    
+            data.forEach(fissure => eras[fissure.tier.toLowerCase()].push(fissure));
+
+            Object.keys(eras).forEach(eraKey => {
+                if(!eras[eraKey][0]) return;
+                const tier = eras[eraKey][0].tier;
+                const fissures = eras[eraKey];
+                let fields = [];
+
+                fissures.forEach(fissure => {
+                    fields.push({
+                        name: `${fissure.missionType} [${fissure.eta}]`,
+                        value: `${fissure.node} [${fissure.enemy}]` // TODO Enemy emoji
+                    });
+                });
+
+                pages.push(
+                    new MessageEmbed()
+                        .setTitle(`${tier} Fissures`)
+                        .addFields(fields)
+                        .setColor(client.colors.primary)
+                        .setThumbnail(client.images.relics[tier.toLowerCase()])
+                        .setTimestamp()
+                        .setFooter(client.user.username, client.user.displayAvatarURL())
+                );
+            });
+
+            pagination(msg, pages, ['◀️', '▶️']);
+
+        } else {
+            msg.channel.send(`${client.emojos.error} **|** ${msg.author.toString()} No response from Warframe API.`)
+        }
+    }
+}
