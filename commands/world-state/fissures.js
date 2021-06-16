d9aec036a4c78f70c37efba1b4ef877a420efb80
Mssjim <mrmssjim@gmail.com>
2021-06-16 11:39:15 -0300

    Update fissures.js

--- commands/world-state/fissures.js
+++ commands/world-state/fissures.js
@@ -10,47 +10,47 @@ module.exports = {
 
     run: async(client, msg, args) => {
         const data = await fecther('https://api.warframestat.us/pc/fissures');
+        
+        if(!data) {
+            msg.channel.send(`${client.emojos.error} **|** ${msg.author.toString()} No response from Warframe API.`)
+            return;
+        }
 
-        if(data) {
-            const pages = [];
-            const eras = {
-                lith: [],
-                meso: [],
-                neo: [],
-                axi: [],
-                requiem: [],
-            };
-    
-            data.forEach(fissure => eras[fissure.tier.toLowerCase()].push(fissure));
+        const pages = [];
+        const eras = {
+            lith: [],
+            meso: [],
+            neo: [],
+            axi: [],
+            requiem: [],
+        };
 
-            Object.keys(eras).forEach(eraKey => {
-                if(!eras[eraKey][0]) return;
-                const tier = eras[eraKey][0].tier;
-                const fissures = eras[eraKey];
-                let fields = [];
+        data.forEach(fissure => eras[fissure.tier.toLowerCase()].push(fissure));
 
-                fissures.forEach(fissure => {
-                    fields.push({
-                        name: `${fissure.missionType} [${fissure.eta}]`,
-                        value: `${fissure.node} [${fissure.enemy}]` // TODO Enemy emoji
-                    });
+        Object.keys(eras).forEach(eraKey => {
+            if(!eras[eraKey][0]) return;
+            const tier = eras[eraKey][0].tier;
+            const fissures = eras[eraKey];
+            let fields = [];
+
+            fissures.forEach(fissure => {
+                fields.push({
+                    name: `${fissure.missionType} [${fissure.eta}]`,
+                    value: `${fissure.node} [${fissure.enemy}]` // TODO Enemy emoji
                 });
-
-                pages.push(
-                    new MessageEmbed()
-                        .setTitle(`${tier} Fissures`)
-                        .addFields(fields)
-                        .setColor(client.colors.primary)
-                        .setThumbnail(client.images.relics[tier.toLowerCase()])
-                        .setTimestamp()
-                        .setFooter(client.user.username, client.user.displayAvatarURL())
-                );
             });
 
-            pagination(msg, pages, ['◀️', '▶️']);
+            pages.push(
+                new MessageEmbed()
+                    .setTitle(`${tier} Fissures`)
+                    .addFields(fields)
+                    .setColor(client.colors.primary)
+                    .setThumbnail(client.images.relics[tier.toLowerCase()])
+                    .setTimestamp()
+                    .setFooter(client.user.username, client.user.displayAvatarURL())
+            );
+        });
 
-        } else {
-            msg.channel.send(`${client.emojos.error} **|** ${msg.author.toString()} No response from Warframe API.`)
-        }
+        pagination(msg, pages, ['◀️', '▶️']);
     }
 }
