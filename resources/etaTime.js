689d49c5c70580867c8d9150c1d27b2055934724
Mssjim <mrmssjim@gmail.com>
2021-06-16 11:37:54 -0300

    Added function to get ETA time

---
+++ resources/etaTime.js
@@ -0,0 +1,35 @@
+module.exports = date => {
+    let millis = date.getTime() - Date.now();
+
+    if (typeof millis !== 'number') {
+        throw new TypeError('millis should be a number');
+    }
+    const eta = [];
+    let seconds = Math.abs(millis / 1000);
+
+    const duration = {
+        "day": 60*60*24,
+        "hour": 60*60,
+        "minute": 60
+    }
+    
+    if (seconds >= duration.day) {
+        eta.push(`${Math.floor(seconds / duration.day)}d`);
+        seconds = Math.floor(seconds) % duration.day;
+    }
+    
+    if (seconds >= duration.hour) {
+        eta.push(`${Math.floor(seconds / duration.hour)}h`);
+        seconds = Math.floor(seconds) % duration.hour;
+    }
+    
+    if (seconds >= duration.minute) {
+        eta.push(`${Math.floor(seconds / duration.minute)}m`);
+        seconds = Math.floor(seconds) % duration.minute;
+    }
+    
+    if (seconds >= 0) {
+        eta.push(`${Math.floor(seconds)}s`);
+    }
+    return eta.join(' ');
+}
