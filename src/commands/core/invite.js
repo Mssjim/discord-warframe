ddc9ccf57ca0327cfeab9919dfae91067e3f4bcf
Mssjim <mrmssjim@gmail.com>
2021-06-18 15:46:15 -0300

    Added 'invite' command

---
+++ src/commands/core/invite.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const colors = require("../../resources/colors.json");
+const emojis = require("../../resources/emojis.json");
+
+module.exports = {
+    name: 'invite',
+    aliases: ['convite', 'convidar', 'add', 'adicionar'],
+    description: 'Get the link to invite me to your server',
+    category: 'core',
+
+    run: async(client, msg, args) => {
+
+        const embed = new MessageEmbed()
+            .setTitle(`${emojis.warframe} Invite`)
+            .setDescription(`**[Click Here to Invite Me!](${client.invite})**`)
+            .setColor(colors.primary)
+            .setTimestamp()
+            .setFooter(client.user.username, client.user.displayAvatarURL())
+        
+        msg.channel.send(msg.author.toString(), embed);
+    }
+}
