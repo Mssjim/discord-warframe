818991f91b1ba6abb0e5c15b249f99bfffc3c9f5
Mssjim <mrmssjim@gmail.com>
2021-06-25 23:12:31 -0300

    Specs update

---
+++ spec/specs/api.spec.js
@@ -0,0 +1,10 @@
+describe("API", () => {
+    const { fetcher } = require('../../src/functions');
+
+    it("should be able to return a response", async() => {
+        const response = await fetcher("http://api.warframestat.us");
+
+        expect(response).toBeInstanceOf(Object);
+        expect(response.code).toBe(200);
+    });
+});
