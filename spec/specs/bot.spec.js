67312f6eaefae4d63d613543af2537e7f7e870e3
Mssjim <mrmssjim@gmail.com>
2021-07-02 16:28:22 -0300

    Add 'bot' specs

---
+++ spec/specs/bot.spec.js
@@ -0,0 +1,17 @@
+describe('Bot', () => {
+    beforeAll(async() => {
+        console.log('\nBot Specs');
+        await client.login(process.env.TOKEN);
+    });
+
+    const { Client } = require('discord.js');
+    const client = new Client();
+    require('dotenv').config();
+
+    describe('client', () => {
+        it('can login', () => {
+            console.log('OKAY');
+            console.log(client.user.tag);
+        });
+    });
+});
